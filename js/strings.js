200fc89fefd787ffda9aff124f6b355f96085b44
mckayythomas <mckaybt@byui.edu>
2022-01-30 00:54:08 -0500

    Week 4 commit

---
+++ js/strings.js
@@ -0,0 +1,14 @@
+document.querySelector('#copyright').textContent = new Date().getFullYear();
+
+function ___() {
+    // INPUT
+    let firstname = getElementByID("firstname");
+    let fruit = getElementByID("fruit");
+    let animal = getElementByID("animal");
+    let animal2 = getElementById("animal2");
+    // PROCESSING
+
+
+    //OUTPUT - Example output: "And it came to pass that the people of ___ did till the land, and raise all manner of ___, and flocks of herds, and flocks of all manner of ___ of every kind, and ___."
+
+}
